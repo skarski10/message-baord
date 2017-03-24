4ce9c82199e3f597fcb38d5316c43ab6eeab3773
Kory <koryskarbek@yahoo.com>
2017-03-24 08:25:56 -0700

    add keys and vaules to models

--- app/models/answer.js
+++ app/models/answer.js
@@ -1,5 +1,9 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-
+  name: DS.attr(),
+  title: DS.attr(),
+  question: DS.attr(),
+  date: DS.attr(),
+  answers: DS.hasMany('answer', { async: true})
 });
--- app/models/question.js
+++ app/models/question.js
@@ -1,5 +1,8 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-
+  name: DS.attr(),
+  answer: DS.attr(),
+  date: DS.attr()
+  question: DS.belongsTo('question', { async: true})
 });
